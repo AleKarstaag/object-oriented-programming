a33dfa06b88a3168f9f7fb27864cdf4f7b32e84b
Alessandro Beninati <alessandro.beninati@ox.ac.uk>
2021-06-09 12:38:48 +0200

    minor

--- example_code/euclid.py
+++ example_code/euclid.py
@@ -1,7 +1,8 @@
 def gcd(a, b):
     """Return the greatest common divisor of a and b using a recursive
     implementation of Euclid's algorithm."""
-    try:
+    try: #keep calling gcd on smaller and smaller values of b
         return gcd(b, a % b)
-    except ZeroDivisionError:
+    except ZeroDivisionError: #untill we get to the point that b is zero
+        #this line will get an exception and we return a
         return a
---
+++ test.py
@@ -0,0 +1,5 @@
+from example_code.polynomial import Polynomial
+
+p=Polynomial((1,2,1))
+
+print(1 + p)
